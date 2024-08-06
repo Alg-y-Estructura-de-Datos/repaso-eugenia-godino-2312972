434eff39c5cc8a6043ca3ec09e1c04005fdb46f8
euge7777 <2312972@ucc.edu.ar>
2024-08-06 19:24:57 -0300

    completado

--- Ej3.cpp
+++ Ej3.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void marcarAsiento (bool *asiento){
+    *asiento = true;  
+}
 
-    cout << "Examen Final Prog 3" << endl;
-
+int main (){
+    const int cant = 10;
+    bool asientos [cant] = {false};
+    int num_asiento;
+    cout << "Estado inicial asientos del teatro: ";
+    for (int i = 0; i < cant ; i++){
+        cout << "Asiento "<<i+1<<": "<<(asientos[i] ? "Reservado":"Libre")<<endl;
+    }
+    cout<<"Ingrese asiento que desea reservar (1 - 10): ";
+    cin >> num_asiento;
+    if (num_asiento < 1 || num_asiento > cant){
+        cout << "Numero asiento invalido" <<endl;
+    }
+    marcarAsiento (&asientos [num_asiento-1]);
+    cout<<"Estados de los asientos despues de la reserva: "<<endl;
+    for (int i = 0; i < cant; i++){
+        cout<<"Asiento "<<i+1<<": "<<(asientos[i] ? "Reservado" : "Libre")<<endl;
+    }
     return 0;
 }
